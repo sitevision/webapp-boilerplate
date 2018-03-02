fdb44f13a5ba7a15d7c1ac2caf90d8f837d2715b
Robin Lagren <robin.lagren@sitevision.se>
2018-03-02 10:56:54 +0100

    updated web app boilerplate docs

--- src/component/Name.js
+++ src/component/Name.js
@@ -2,11 +2,16 @@ define(function(require) {
    'use strict';
 
    var
+      _          = require('underscore'),
       Component  = require('Component'),
       template   = require('/template/name');
 
    return Component.extend({
 
-      template: template
+      template: template,
+
+      filterState: function(state) {
+         return _.extend({}, {name: state.name});
+      }
    });
 });
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ define(function(require) {
    'use strict';
 
    var
+      _          = require('underscore'),
       Component  = require('Component'),
       template   = require('/template/main');
 
    return Component.extend({
 
-      template: template
+      template: template,
+
+      filterState: function(state) {
+         return _.extend({}, {message: state.message});
+      }
    });
 });
